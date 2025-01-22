d6be9a1d894078b99622295844ade68617a8e6d4
Sean Zhang <sean@naesna.es>
2025-01-22 18:57:09 -0500

    feat: window-switcher

---
+++ Casks/window-switcher.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Cask-Cookbook
+#                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
+cask "window-switcher" do
+  version "0.2.1"
+  sha256 "44b5a95b62d1ab3a39aa7b62497366b19a5883bcf2bae1152d248c67243f86cb"
+
+  url "https://github.com/sean01zhang/window-switcher/releases/download/v#{version}/window-switcher-v#{version}.zip"
+  name "window-switcher"
+  desc "Window Switcher is a simple searchable window switcher for macOS"
+  homepage "https://github.com/sean01zhang/window-switcher"
+
+  # Documentation: https://docs.brew.sh/Brew-Livecheck
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"] || release["prerelease"]
+
+        match = release["tag_name"]&.match(regex)
+        next if match.blank?
+
+        match[1]
+      end
+    end
+  end
+
+  depends_on macos: "12.3"
+
+  app "Window Switcher"
+
+  # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
+  # No zap stanza required
+end
