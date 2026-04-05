8713a54e3d03451d7c701ea6185dac1273fafe2e
Anka <runner@sjc22-bm206-2d13fc9c-9bb7-480d-9962-5a1e377aa0d1-3E2A31BD28CC.local>
2026-04-05 13:05:19 +0000

    window-switcher 0.5.2

--- Casks/window-switcher.rb
+++ Casks/window-switcher.rb
@@ -1,6 +1,6 @@
 cask "window-switcher" do
-  version "0.5.1"
-  sha256 "85f9ea77d5128f813aec8ded63e148a6e36ffe3fa747ca4daae08b647ccd34e7"
+  version "0.5.2"
+  sha256 "03ed0e359c47c992503a284e5e716d400ecae5c9307c18a70a6bf847d7408c8b"
 
   url "https://github.com/sean01zhang/window-switcher/releases/download/v#{version}/window-switcher-v#{version}.zip"
   name "window-switcher"
